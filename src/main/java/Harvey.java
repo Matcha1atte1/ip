2d9a112e32791388411a26e2ef76f03c6ba10686
Jovan Lim <jovanlim360@gmail.com>
2026-08-16 23:19:08 +0800

    Add greeting

--- src/main/java/Harvey.java
+++ src/main/java/Harvey.java
@@ -1,13 +1,25 @@
 /**
  * Entry point of the Harvey chatbot.
+ * At this stage Harvey only greets the user and exits immediately.
  */
 public class Harvey {
+    /** Horizontal line used to separate Harvey's replies from the user's input. */
+    private static final String DIVIDER = "____________________________________________________________";
+
+    /** ASCII art of the chatbot's name, shown once on startup. */
+    private static final String BANNER = " _   _     _     ____  __     __ _____ __   __\n"
+            + "| | | |   / \\   |  _ \\ \\ \\   / /| ____|\\ \\ / /\n"
+            + "| |_| |  / _ \\  | |_) | \\ \\ / / |  _|   \\ V / \n"
+            + "|  _  | / ___ \\ |  _ <   \\ V /  | |___   | |  \n"
+            + "|_| |_|/_/   \\_\\|_| \\_\\   \\_/   |_____|  |_|  ";
+
     public static void main(String[] args) {
-        String banner = " _   _     _     ____  __     __ _____ __   __\n"
-                + "| | | |   / \\   |  _ \\ \\ \\   / /| ____|\\ \\ / /\n"
-                + "| |_| |  / _ \\  | |_) | \\ \\ / / |  _|   \\ V / \n"
-                + "|  _  | / ___ \\ |  _ <   \\ V /  | |___   | |  \n"
-                + "|_| |_|/_/   \\_\\|_| \\_\\   \\_/   |_____|  |_|  \n";
-        System.out.println(banner);
+        System.out.println(DIVIDER);
+        System.out.println(BANNER);
+        System.out.println("Hello! I'm Harvey.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
