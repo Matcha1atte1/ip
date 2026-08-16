5ed25650e46dd569776719f9e1625b71eaaa1aa7
Jovan Lim <jovanlim360@gmail.com>
2026-08-16 23:13:55 +0800

    Rename chatbot Duke -> Harvey

---
+++ src/main/java/Harvey.java
@@ -0,0 +1,13 @@
+/**
+ * Entry point of the Harvey chatbot.
+ */
+public class Harvey {
+    public static void main(String[] args) {
+        String banner = " _   _     _     ____  __     __ _____ __   __\n"
+                + "| | | |   / \\   |  _ \\ \\ \\   / /| ____|\\ \\ / /\n"
+                + "| |_| |  / _ \\  | |_) | \\ \\ / / |  _|   \\ V / \n"
+                + "|  _  | / ___ \\ |  _ <   \\ V /  | |___   | |  \n"
+                + "|_| |_|/_/   \\_\\|_| \\_\\   \\_/   |_____|  |_|  \n";
+        System.out.println(banner);
+    }
+}
